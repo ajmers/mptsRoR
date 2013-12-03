14ec01faf19a5d34e8bac922449d6676638481ad
Anne Maiale <amaiale@gmail.com>
2013-12-03 01:03:00 -0500

    interior pages

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,17 @@
 class ActivitiesController < ApplicationController
+
+	def new
+		@activity = Activity.new
+	end
+
+	def create
+		@user = User.find(params[:user_id])
+	end 
+
+	def show
+	end
+
+	def index
+	end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ def create
 	user = User.find_by(email: params[:session][:email].downcase)
 	if user && user.authenticate(params[:session][:password])
 		sign_in user
-		redirect_to user
+		render "activities/index"
 		# Sign the user in and redirect to the user's show page.
 	else
 		#Create an error message and re-render the signin form.
